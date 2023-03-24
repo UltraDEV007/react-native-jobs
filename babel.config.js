1c9c342180d38145dce79d0526a2d1f7904550db
UltraDEV007 <LeTuan2024@outlook.com>
2023-03-24 15:50:55 +0530

    env configured

--- babel.config.js
+++ babel.config.js
@@ -5,6 +5,17 @@ module.exports = function (api) {
     plugins: [
       "@babel/plugin-proposal-export-namespace-from",
       "react-native-reanimated/plugin",
+      [
+        "module:react-native-dotenv",
+        {
+          moduleName: "@env",
+          path: ".env",
+          blacklist: null,
+          whitelist: null,
+          safe: false,
+          allowUndefined: true,
+        },
+      ],
       require.resolve("expo-router/babel"),
     ],
   };
